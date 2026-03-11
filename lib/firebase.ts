8dda7e012dd3c3d6bbd4e8a013129c4c353f0314
GJordy98 <glennjordy1998@gmail.com>
2026-03-11 16:37:23 +0100

    feat: modifications push vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getMessaging, type Messaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDttvXdza5YXxz84r-5BIfcvy7Xhbvmb-Y',
-    authDomain: 'e-dr-pharma-fcm.firebaseapp.com',
-    projectId: 'e-dr-pharma-fcm',
-    storageBucket: 'e-dr-pharma-fcm.firebasestorage.app',
-    messagingSenderId: '42167148530',
-    appId: '1:42167148530:web:e9fa630f803fa0469c156b',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
